ae00f2401161f4626bade1296ec3db709dc44531
kasiriveni <kasiriveni@gmail.com>
2019-02-12 18:48:27 +0530

    udpate the v9

--- fragement-cli/bin/fragement-cli.js
+++ fragement-cli/bin/fragement-cli.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
 
 "use strict";
-require("../index.js")();
+const program = require("commander");
+const { createFragement } = require("../utils/index");
+
+program
+    .version("1.0.4")
+    .description("Create custom fragement")
+    .option(
+        "-a  --add <fragementname>",
+        "create custom fragement",
+        createFragement
+    );
+
+program.parse(process.argv);
+
+console.log(" list: %j", program.createFragement);
